c46e91a3e6baa23239825d2bca1282298ffe73d9
DylanAlloy <alloydylan@icloud.com>
2023-12-11 08:59:47 -0500

    🚧 fix(build): export setting & syntax

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>We're building a world where&nbsp;</h1>
+				<h1 className={title()}>We&apos;re building a world where&nbsp;</h1>
 				<h1 className={title({ color: "violet" })}>intelligence&nbsp;</h1>
 				<br />
 				<h1 className={title()}>
@@ -43,7 +43,7 @@ export default function Home() {
 			<div className="mt-8">
 				<Snippet hideSymbol hideCopyButton variant="flat">
 					<span>
-						> follow the <Code color="primary">🐇</Code>
+						follow the <Code color="primary">🐇</Code>
 					</span>
 				</Snippet>
 			</div>
