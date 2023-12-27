f7ac938844a5716e3c8ebea42a5035300a30d5d6
DylanAlloy <alloydylan@icloud.com>
2023-12-27 09:18:00 -0500

    🚧 fix(wip): navbar name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -71,8 +71,7 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">Digits</p>
-						<small>by PrismadicAI</small>
+						<p className="font-bold text-inherit">Prismadic</p>
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
