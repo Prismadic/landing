2720a69e9050b042e53997eb9f0938f7e15d8a33
DylanAlloy <alloydylan@icloud.com>
2024-03-29 07:21:23 -0400

    🚧 fix(wip): messaging

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,11 @@ export default function Home() {
 						subscribe on <Link href="https://prismadic.substack.com">substack</Link>
 					</span>
 				</Snippet>
+				<Snippet hideSymbol hideCopyButton variant="flat">
+					<span>
+						join the <Link href="https://discord.gg/prismadicai">discord</Link>
+					</span>
+				</Snippet>
 			</div>
 		</section>]
 	);
